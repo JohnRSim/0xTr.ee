fe4eeba8a61ec6e27041ac3ddaec662eec5a2ba2
John Sim <jsim@fishbowlsolutions.com>
2022-02-03 18:27:28 +0000

    cleanup layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -16,40 +16,16 @@
   import '$lib/styles/normalise.css';
   import '$lib/styles/index.scss';
 
-  import Footer from '$lib/components/Footer.svelte';
-  import Header from '$lib/components/Header.svelte';
   import PWA from '$lib/components/PWA.svelte';
 </script>
 
 <PWA />
+
 <div class="container">
-  <Header />
   <main class="main-container">
     <slot />
   </main>
-  <Footer />
 </div>
 
 <style lang="scss">
-  .container {
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: column;
-    justify-content: space-between;
-    padding: $spacing-4 $spacing-0 $spacing-0;
-    margin: $spacing-0 auto;
-    min-height: 100vh;
-  }
-
-  .main-container {
-    max-width: $max-width-wrapper;
-    width: 100%;
-    margin: $spacing-0 auto;
-  }
-
-  @media screen and (max-width: $desktop-breakpoint) {
-    .container {
-      margin: $spacing-0 $spacing-4;
-    }
-  }
 </style>
